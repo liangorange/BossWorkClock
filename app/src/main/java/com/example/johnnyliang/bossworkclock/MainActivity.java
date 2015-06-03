e7de79dd29cafc2ecf2619d907a9c8cfa6e5e3bb
JohnnyLiang <liangorange@ymail.com>
2015-06-03 12:22:12 -0600

    Johnny Made first comment

--- app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    // Johnny think this app will awesome
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
