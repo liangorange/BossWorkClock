261548a12361389ae3a5fd3542a0d8a8d06dc774
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-06 02:13:58 -0600

    connected buttons and added a blue background

--- app/src/main/java/com/example/johnnyliang/bossworkclock/Employee.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/Employee.java
@@ -10,17 +10,23 @@ public class Employee {
     private Time dailyTotal;
     private Time weeklyTotal;
     private Time monthlyTotal;
+    private TimeTracker timeTracker;
 
     //Getters and setters
-    public String getName() { return name; }
-    public Time getDailyTotal() { return dailyTotal; }
-    public Time getWeeklyTotal() { return weeklyTotal; }
-    public Time getMonthlyTotal() { return monthlyTotal; }
-    public void setName(String name) { this.name = name; }
-    public void setDailyTotal(Time dailyTotal) { this.dailyTotal = dailyTotal; }
-    public void setWeeklyTotal(Time weeklyTotal) { this.weeklyTotal = weeklyTotal; }
-    public void setMonthlyTotal(Time monthlyTotal) { this.monthlyTotal = monthlyTotal; }
+    public String getName()             { return name;         }
+    public Time getDailyTotal()         { return dailyTotal;   }
+    public Time getWeeklyTotal()        { return weeklyTotal;  }
+    public Time getMonthlyTotal()       { return monthlyTotal; }
+    public TimeTracker getTimeTracker() { return timeTracker;  }
 
+    public void setName(String name)                    { this.name = name;                 }
+    public void setDailyTotal(Time dailyTotal)          { this.dailyTotal = dailyTotal;     }
+    public void setWeeklyTotal(Time weeklyTotal)        { this.weeklyTotal = weeklyTotal;   }
+    public void setMonthlyTotal(Time monthlyTotal)      { this.monthlyTotal = monthlyTotal; }
+    public void setTimeTracker(TimeTracker timeTracker) { this.timeTracker = timeTracker;   }
+
+    //public Employee() { name = ""; dailyTotal = new Time(); weeklyTotal = new Time();
+     //                   monthlyTotal = new Time(); timeTracker = new TimeTracker(); }
     public void sendUpdate() {
 
     }
