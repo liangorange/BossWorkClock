500dba09dbf3c863e38d80b753f9509ae502c85b
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-06 02:11:59 -0600

    connected buttons and added a blue background

--- app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
@@ -4,18 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Employee employee;
 
-    // Johnny tried to get merge conflict
-
-//Jonathan was here
-//gerald
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // employee = new Employee();
     }
 
     @Override
@@ -38,5 +39,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addPunchIn(View view) {
+        Toast.makeText(MainActivity.this,"Added a Punched In", Toast.LENGTH_SHORT).show();
+    }
+
+    public void addPunchOut(View view) {
+        Toast.makeText(MainActivity.this,"Added a Punched Out", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void punchIn(View view){
+        //employee.getTimeTracker().getClockInLocation();
+        //employee.getTimeTracker().clockIn();
+        Toast.makeText(MainActivity.this,"Punched In", Toast.LENGTH_SHORT).show();
+    }
+
+    public void punchOut(View view){
+       // employee.getTimeTracker().getClockOutLocation();
+        //employee.getTimeTracker().clockOut();
+        Toast.makeText(MainActivity.this,"Punched Out", Toast.LENGTH_SHORT).show();
     }
 }
