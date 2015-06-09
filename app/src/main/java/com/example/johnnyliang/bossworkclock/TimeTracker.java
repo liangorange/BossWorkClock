51fa0749755c55820652ce16595736a750add509
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-09 17:59:24 -0600

    started get location function

--- app/src/main/java/com/example/johnnyliang/bossworkclock/TimeTracker.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/TimeTracker.java
@@ -2,26 +2,36 @@ package com.example.johnnyliang.bossworkclock;
 
 import java.sql.Time;
 
+class GPSCoord {
+    public double longitude;
+    public double latitude;
+};
+
 /**
  * Created by Gerald on 6/5/2015.
  */
 public class TimeTracker {
     private Time clockInTime;
     private Time clockOutTime;
-    private String clockInLocation;
+    private GPSCoord clockInLocation;
     private String clockOutLocation;
 
     // Getters and setters
     public Time getClockInTime(){ return clockInTime; }
     public Time getClockOutTime() { return clockOutTime; }
-    public String getClockInLocation() { return clockInLocation; }
+    public GPSCoord getClockInLocation() { return clockInLocation; }
     public String getClockOutLocation() { return clockOutLocation; }
     public void setClockInTime(Time clockInTime) { this.clockInTime = clockInTime; }
     public void setClockOutTime(Time clockOutTime) { this.clockOutTime = clockOutTime; }
-    public void setClockInLocation(String clockInLocation) {
-        this.clockInLocation = clockInLocation; }
+
+    public void setClockInLocation(GPSCoord clockInLocation) {
+        this.clockInLocation.latitude = clockInLocation.latitude;
+        this.clockInLocation.longitude = clockInLocation.longitude;
+    }
+
     public void setClockOutLocation(String clockOutLocation) {
-        this.clockOutLocation = clockOutLocation; }
+        this.clockOutLocation = clockOutLocation;
+    }
 
     public void clockIn() {
 
@@ -31,11 +41,11 @@ public class TimeTracker {
 
     }
 
-    public void addClockIn() {
+    public void editClockIn() {
 
     }
 
-    public void addClockOut() {
+    public void editClockOut() {
 
     }
 }
