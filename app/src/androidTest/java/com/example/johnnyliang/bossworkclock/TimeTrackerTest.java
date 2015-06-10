7a749138086a4f84bb932dbde8ba85dd48b0874e
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-10 12:13:31 -0600

    test class

--- app/src/androidTest/java/com/example/johnnyliang/bossworkclock/TimeTrackerTest.java
+++ app/src/androidTest/java/com/example/johnnyliang/bossworkclock/TimeTrackerTest.java
@@ -1,7 +1,11 @@
 package com.example.johnnyliang.bossworkclock;
 
+import android.test.InstrumentationTestCase;
+
 /**
  * Created by Gerald on 6/10/2015.
  */
-public class TimeTrackerTest {
+public class TimeTrackerTest extends InstrumentationTestCase {
+    public void
+    
 }
