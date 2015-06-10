53a0572bba417d52f64f00c25c26c00e06ad612b
yhkwok <kwo13001@byui.edu>
2015-06-10 12:36:13 -0600

    clockOutLocation Test

--- app/src/androidTest/java/com/example/johnnyliang/bossworkclock/TimeTrackerTest.java
+++ app/src/androidTest/java/com/example/johnnyliang/bossworkclock/TimeTrackerTest.java
@@ -6,6 +6,18 @@ import android.test.InstrumentationTestCase;
  * Created by Gerald on 6/10/2015.
  */
 public class TimeTrackerTest extends InstrumentationTestCase {
-    public void
+    public void testClockOutLocation() {
+        TimeTracker tt = new TimeTracker();
+
+        tt.setClockOutLocation("Here");
+        assertEquals("Here", tt.getClockOutLocation());
+
+        tt.setClockOutLocation("There");
+        assertEquals("There", tt.getClockOutLocation());
+
+        tt.setClockOutLocation("No where");
+        assertEquals("No where", tt.getClockOutLocation());
+    }
+
     
 }
