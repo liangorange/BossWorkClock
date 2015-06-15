5436b0901b5ed2c6d6bef4a0f52c90b6785d244a
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-15 11:46:34 -0600

    set

--- app/src/main/java/com/example/johnnyliang/bossworkclock/TimeTracker.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/TimeTracker.java
@@ -1,64 +1,28 @@
 package com.example.johnnyliang.bossworkclock;
 
 import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
-
-/*class GPSCoord extends FragmentActivity implements LocationListener{
-    public double longitude;
-    public double latitude;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        /* Use the LocationManager class to obtain GPS locations
-        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-    }
-
-    LocationListener mlocListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Toast.makeText(getApplicationContext(), "status changed", Toast.LENGTH_SHORT).show();
-        }
-        @Override
-        public void onProviderEnabled(String provider) {
-            Toast.makeText(getApplicationContext(), "GPS Enabled", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Toast.makeText(getApplicationContext(), "GPS Disabled", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mlocListener);
-};*/
-
- //class GPSCoord {
-   // public double longitude;
-    //public double latitude;
-//}
 /**
  * Created by Gerald on 6/5/2015.
  */
 public class TimeTracker {
-    private Time clockInTime;
-    private Time clockOutTime;
+    private String clockInTime;
+    private String clockOutTime;
     private GPSCoord clockInLocation;
     private GPSCoord clockOutLocation;
 
     // Getters and setters
-    public Time getClockInTime(){ return clockInTime; }
-    public Time getClockOutTime() { return clockOutTime; }
+    public String getClockInTime(){ return clockInTime; }
+    public String getClockOutTime() { return clockOutTime; }
     public GPSCoord getClockInLocation() { return clockInLocation; }
     public GPSCoord getClockOutLocation() { return clockOutLocation; }
-    public void setClockInTime(Time clockInTime) { this.clockInTime = clockInTime; }
-    public void setClockOutTime(Time clockOutTime) { this.clockOutTime = clockOutTime; }
+    public void setClockInTime(String clockInTime) { this.clockInTime = clockInTime; }
+    public void setClockOutTime(String clockOutTime) { this.clockOutTime = clockOutTime; }
 
     public void setClockInLocation(GPSCoord clockInLocation) {
         this.clockInLocation.latitude = clockInLocation.latitude;
@@ -72,10 +36,17 @@ public class TimeTracker {
 
     public void clockIn() {
         //keep track of time
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        setClockInTime(dateFormat.format(date)); //15:59
+       // System.out.println(dateFormat.format(date));
+
     }
 
     public void clockOut() {
-
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        setClockOutTime(dateFormat.format(date));//15:59
     }
 
     public void editClockIn() {
