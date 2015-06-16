5815d1fe0250ce118dabce86e106739be65b5d65
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-15 23:10:05 -0600

    tried to add name but isn't working totally yet

--- app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.johnnyliang.bossworkclock;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.sql.Time;
-
+import android.content.DialogInterface.OnKeyListener;
 
 public class MainActivity extends ActionBarActivity {
     private Employee employee;
+  //  private FileOutputStream out;
+    private PrintWriter out;
+    private PrintStream in;
+    private String filename = "name.txt";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +37,9 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         employee = new Employee();
+        this.readName();
+
+
     }
 
     @Override
@@ -50,6 +70,53 @@ public class MainActivity extends ActionBarActivity {
 
     public void editPunchOut(View view) {
         Toast.makeText(MainActivity.this,"Added a Punched Out", Toast.LENGTH_SHORT).show();
+    }
+    
+
+    public String getName() {
+        //String name="";
+      setContentView(R.layout.activity_main);
+        //EditText theName = (EditText) findViewById(R.id.name);
+        //String name = theName.getText().toString();
+        TextView theName = (TextView) findViewById(R.id.name);
+        String name = theName.getText().toString();
+        theName.setText(name);
+        Toast.makeText(MainActivity.this,"here1", Toast.LENGTH_SHORT).show();
+        return name;
+    }
+    /**
+     *
+     */
+    public void saveName(View view) {
+         String name = this.getName();
+            try {
+                BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+                Toast.makeText(MainActivity.this,"here2", Toast.LENGTH_SHORT).show();
+                out.write(name);
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        this.readName();
+    }
+
+    public void readName() {
+        TextView textView = (TextView) findViewById(R.id.name);
+        String name= "bob";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+
+
+            //takes one line at a time and sets the topic from it
+            while ((line = reader.readLine()) != null) {
+                name += line;
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        textView.setText(name);
     }
 
     /**
