d81c5f593c99c62f99d8637243be4d5a330d6d6c
JohnnyLiang <liangorange@ymail.com>
2015-06-15 21:23:37 -0600

    Add Time clock and Time counter

--- app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.johnnyliang.bossworkclock;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,10 +11,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
     private Employee employee;
+
+    private String dateFormat;
+
+    private Date startingDate;
+
+    private TimeCount count = new TimeCount();
+
+
+    Handler startHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            getTimeString();
+            //displayStart();
+            System.out.println("Call handler");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +43,62 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         employee = new Employee();
+
+        count.setActivity(this);
+    }
+
+
+    public void doWork() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try {
+                    System.out.println("For testing");
+                    TextView statusView = (TextView) findViewById(R.id.status);
+                    Date dt = new Date();
+                    long diff = dt.getTime() - startingDate.getTime();
+                    long seconds = diff / 1000 % 60;
+                    long minutes = diff / (60 * 1000) % 60;
+                    long hours = diff / (60 * 60 * 1000) % 60;
+
+                    String secondFormat = String.format("%02d", seconds);
+                    String minuteFormat = String.format("%02d", minutes);
+                    String hourFormat = String.format("%02d", hours);
+
+                    String curTime = hourFormat + ":" + minuteFormat + ":" + secondFormat;
+
+                    String status = "                Punched In";
+                    String s = "Work start time: " + getTimeString();
+                    String s2 = "Time worked today: " + curTime;
+
+                    String s3 = status + "\n" + s + "\n" + s2;
+                    statusView.setTextColor(0xff18ff1a);
+                    statusView.setText(s3);
+                } catch (Exception e) {
+                }
+            }
+        });
+    }
+
+    public String getTimeString() {
+        Date date = new Date();
+
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+
+        Calendar cal = Calendar.getInstance();
+
+        dateFormat = df.format(startingDate);
+
+        return dateFormat;
+    }
+
+    /*
+    public void displayStart(String text) {
+        startView.setText(text);
+    }
+    */
+
+    public void displayStartTotal() {
+        startHandler.sendEmptyMessage(0);
     }
 
     @Override
@@ -58,17 +137,32 @@ public class MainActivity extends ActionBarActivity {
      */
     public void punchIn(View view){
 
+        startingDate = new Date();
+
+        Thread loadThread = new Thread(count);
+        loadThread.start();
+
+
         // Can't punch in if you already are punched in
         if(!employee.getPunchedIn()) {
             employee.setPunchedIn(true);
             employee.getTimeTracker().getClockInLocation();
             employee.getTimeTracker().clockIn();
 
+            /*
             String t1;// = new Time(0, 0, 0);
             t1 = employee.getTimeTracker().getClockInTime();
 
             Time t2 = new Time(0, 0, 0);
+            */
 
+            /*
+            startingDate = new Date();
+
+            Thread loadThread = new Thread(count);
+            loadThread.start();
+            */
+            /*
             String status = "                Punched In";
             String s = "Work start time: " + t1;
             String s2 = "Time worked today: ";//+ current time - t1;
@@ -76,7 +170,7 @@ public class MainActivity extends ActionBarActivity {
             TextView textView = (TextView) findViewById(R.id.status);
             textView.setTextColor(0xff18ff1a);
             textView.setText(s3);
-
+            */
             Toast.makeText(MainActivity.this,"Punched in", Toast.LENGTH_SHORT).show();
 
         }
