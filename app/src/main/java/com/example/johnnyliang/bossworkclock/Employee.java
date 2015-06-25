1f1cd2720f1cee3a2b844dc9092c955d54a9b4a4
gmurph52 <bobsmith52bananas@yahoo.com>
2015-06-25 01:39:46 -0600

    The table display is now correct (still need to change time to hours instead of seconds) keeps track of hours per day/week/month. You can close out of it and will remember your times.

--- app/src/main/java/com/example/johnnyliang/bossworkclock/Employee.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/Employee.java
@@ -1,41 +1,44 @@
 package com.example.johnnyliang.bossworkclock;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
 /**
  * Created by Gerald on 6/5/2015.
  */
 public class Employee {
     private String name;
-    private double dailyTotal;
-    private double weeklyTotal;
-    private double monthlyTotal;
+    private float dailyTotal = 0;
+    private float weeklyTotal = 0;
+    private float monthlyTotal = 0;
+
     private boolean punchedIn;
-    private double totalHour = 0;
     private String clockInTime;
     private String clockOutTime;
     private GPSCoord clockInLocation;
     private GPSCoord clockOutLocation;
 
     //Getters and setters
-    public double getDailyTotal()         { return dailyTotal;       }
-    public double getWeeklyTotal()        { return weeklyTotal;      }
-    public double getMonthlyTotal()       { return monthlyTotal;     }
-    public boolean getPunchedIn()         { return punchedIn;        }
-    public double getTotalHour()          { return totalHour;        }
+    public float getDailyTotal()         { return dailyTotal;       }
+    public float getWeeklyTotal()        { return weeklyTotal;      }
+    public float getMonthlyTotal()       { return monthlyTotal;     }
+
+    public boolean getPunchedIn()        { return punchedIn;        }
     public String getClockInTime()        { return clockInTime;      }
     public String getClockOutTime()       { return clockOutTime;     }
     public GPSCoord getClockInLocation()  { return clockInLocation;  }
     public GPSCoord getClockOutLocation() { return clockOutLocation; }
 
 
-    public void setName(String name)                 { this.name = name;                 }
-    public void setDailyTotal(double dailyTotal)     { this.dailyTotal = dailyTotal;     }
-    public void setWeeklyTotal(double weeklyTotal)   { this.weeklyTotal = weeklyTotal;   }
-    public void setMonthlyTotal(double monthlyTotal) { this.monthlyTotal = monthlyTotal; }
+    public void setName(String name)        { this.name = name;     }
+
+
+    public void incDailyTotal(double hour)   { dailyTotal += hour;   }
+    public void incWeeklyTotal(double hour)  { weeklyTotal += hour;  }
+    public void incMonthlyTotal(double hour) { monthlyTotal += hour; }
+
+    public void setDailyTotal(double hour)   { dailyTotal += hour;   }
+    public void setWeeklyTotal(double hour)  { weeklyTotal += hour;  }
+    public void setMonthlyTotal(double hour) { monthlyTotal += hour; }
+
+
     public void setPunchedIn(boolean punchedIn)      { this.punchedIn = punchedIn;       }
     public void setClockInTime(String clockInTime)   { this.clockInTime = clockInTime;   }
     public void setClockOutTime(String clockOutTime) { this.clockOutTime = clockOutTime; }
@@ -50,9 +53,7 @@ public class Employee {
         this.clockOutLocation.longitude = clockOutLocation.longitude;
     }
 
-    public void setTotalHour(double hour) {
-        totalHour += hour;
-    }
+
 
     public String getName() {
 
@@ -65,17 +66,17 @@ public class Employee {
     public void clockIn() {
 
         //keep track of time
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        Date date = new Date();
-        setClockInTime(dateFormat.format(date)); //15:59
+      //  DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+       // Date date = new Date();
+        //setClockInTime(dateFormat.format(date)); //15:59
         // System.out.println(dateFormat.format(date));
 
     }
 
     public void clockOut() {
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        Date date = new Date();
-        setClockOutTime(dateFormat.format(date));//15:59
+      //  DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+       // Date date = new Date();
+        //setClockOutTime(dateFormat.format(date));//15:59
     }
 
     public void editClockIn() {
--- app/src/main/java/com/example/johnnyliang/bossworkclock/TimeCount.java
+++ app/src/main/java/com/example/johnnyliang/bossworkclock/TimeCount.java
@@ -1,11 +1,14 @@
 package com.example.johnnyliang.bossworkclock;
 
+import android.util.Log;
+
 /**
  * Created by JohnnyLiang on 6/15/15.
  */
 public class TimeCount implements Runnable {
     MainActivity activity;
     Employee employee;
+    public final static String TAG = "TimeCount";
 
     public void setActivity(MainActivity myActivity) {
         activity = myActivity;
@@ -20,12 +23,15 @@ public class TimeCount implements Runnable {
         while(!Thread.currentThread().isInterrupted() && employee.getPunchedIn()){
             try {
                 activity.doWork();
-                employee.setTotalHour(0.01);
+                employee.incDailyTotal(0.01);
+                employee.incWeeklyTotal(0.01);
+                employee.incMonthlyTotal(0.01);
                 Thread.sleep(1000); // Pause of 1 Second
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }catch(Exception e){
+                Log.e(TAG,"The background run thread is not working", e);
             }
         }
-    }  
+    }
 }
